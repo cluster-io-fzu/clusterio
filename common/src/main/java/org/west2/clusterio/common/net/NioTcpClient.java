c7fca348f012ef18856ee9cedf05d3baa4785333
Benxinm <yn03.zhang@gmail.com>
2023-11-21 21:43:55 +0800

    feat: netty client reconnect

--- common/src/main/java/org/west2/clusterio/common/net/NioTcpClient.java
+++ common/src/main/java/org/west2/clusterio/common/net/NioTcpClient.java
@@ -1,37 +1,49 @@
 package org.west2.clusterio.common.net;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
+import java.util.concurrent.TimeUnit;
 
-public class NioTcpClient implements Peer{
+
+public class NioTcpClient implements Peer {
     private final String ip;
     private final int port;
     private int eventExecutorsThreads;
-    private ChannelHandler channelHandler;
+    private ChannelInitializer<NioSocketChannel> channelInitializer;
     private NioEventLoopGroup eventExecutors;
     private Bootstrap bootstrap;
-    private ChannelFuture channelFuture;
+    private Channel channel;
 
-    public NioTcpClient(String ip, int port,ChannelHandler channelHandler) {
+    public NioTcpClient(String ip, int port, ChannelInitializer<NioSocketChannel> channelInitializer) {
         this.ip = ip;
         this.port = port;
-        this.channelHandler = channelHandler;
+        this.channelInitializer = channelInitializer;
     }
 
-    public NioTcpClient(String ip, int port, int eventExecutorsThreads, ChannelHandler channelHandler) {
+    public NioTcpClient(String ip, int port, int eventExecutorsThreads, ChannelInitializer<NioSocketChannel> channelInitializer) {
         this.ip = ip;
         this.port = port;
         this.eventExecutorsThreads = eventExecutorsThreads;
-        this.channelHandler = channelHandler;
+        this.channelInitializer = channelInitializer;
     }
 
     @Override
     public void shutdown() throws Exception {
-
+        try {
+            if (channel != null) {
+                channel.closeFuture().sync();
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            eventExecutors.shutdownGracefully();
+        }
     }
 
     @Override
@@ -41,11 +53,28 @@ public class NioTcpClient implements Peer{
             bootstrap = new Bootstrap();
             bootstrap.group(eventExecutors)
                     .channel(NioSocketChannel.class)
-                    .handler(channelHandler);
-            channelFuture = bootstrap.connect(ip, port).sync();
-            channelFuture.channel().closeFuture().sync();
-        }finally {
-            eventExecutors.shutdownGracefully();
+                    .handler(channelInitializer);
+            doConnect();
+        } catch (Exception e) {
+            throw e;
         }
+    }
+
+    /**
+     * client reconnect to server after network failure
+     */
+    private void doConnect() {
+        if (channel != null && channel.isActive()) {
+            return;
+        }
+        ChannelFuture future = bootstrap.connect(ip, port);
+        future.addListener((ChannelFutureListener) futureListener -> {
+            if (futureListener.isSuccess()) {
+                channel = futureListener.channel();
+                //log
+            } else {
+                futureListener.channel().eventLoop().schedule(() -> doConnect(), 3, TimeUnit.SECONDS);
+            }
+        });
     }
 }
